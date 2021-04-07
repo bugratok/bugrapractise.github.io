bfba0f4d3665884066699682b57094ea45846aa0
bugratok <bugratok@users.noreply.github.com>
2021-04-07 20:18:47 +0300

    1.5

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-let hamburger = document.getElementById('label');
-let main = document.getElementById('main');
+let hamburger = document.getElementById('check');
+let nav = document.getElementById('nav');
+let span1 = document.querySelector('.line1');
+let span2 = document.querySelector('.line2');
+let span3 = document.querySelector('.line3');
 
 hamburger.addEventListener('click', function(){
-    main.classList.toggle("active");
+    nav.classList.toggle("open");
+    span1.classList.toggle("open");
+    span2.classList.toggle("open");
+    span3.classList.toggle("open");
 });
